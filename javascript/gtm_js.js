716a83dde7552643184bd81b0c5725155b35d335
Adam <41493452+afl5082@users.noreply.github.com>
2019-10-17 13:20:00 -0400

    Update and rename client-work to gtm_js.js

---
+++ javascript/gtm_js.js
@@ -0,0 +1,11 @@
+// extracting resource level from hhmi (biointeractive) to place in custom dimension within Google Analytics 
+
+function levels() {
+x = document.querySelector("#main > div.l-content > div > div.l-constrain > div > div > div.resource-header > div.resource-header__row > div.resource-header__meta > div.resource-header__grade-level").innerText;
+  
+c = x.replace(/\n/g, ";"); 
+b =  c.replace("Level;","");
+ 
+return b
+  
+}
