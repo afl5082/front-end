e68b65e3272358f851ed96b4e96242b43ea4cb38
Adam <41493452+afl5082@users.noreply.github.com>
2019-10-17 13:22:16 -0400

    Create barr_gtm.js

---
+++ javascript/barr_gtm.js
@@ -0,0 +1,17 @@
+// extracting author out of barrfoundation blog posts, if author isn't present, return null
+
+function() {
+  
+  var e = document.querySelector("head meta:nth-child(5)").name;
+  var b = document.querySelector("head meta:nth-child(5)").content;
+  
+  if (e != "author"){
+  return "null";   }
+  
+  
+   else {
+     return b;
+  }
+  
+}
+  
