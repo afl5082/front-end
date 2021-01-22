e607eea63eb99b1ddc3d2813a32f83dc61b889c1
Adam <41493452+afl5082@users.noreply.github.com>
2021-01-22 16:12:50 -0500

    Update and rename vilcek to vilcek.js

---
+++ javascript/vilcek.js
@@ -0,0 +1,8 @@
+// locating country on map click, hooking into Google Tag Manager click element, country named stored in the first nodeValue, extracting first three letters (USA,CAN..) via Regex match
+
+function () {
+  
+   country = google_tag_manager["GTM-NFF8XL"].dataLayer.get("gtm.element").attributes[0].nodeValue;
+   return country.match(/^.{0,3}/)[0];
+  
+}
